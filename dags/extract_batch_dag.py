fbf27413ba548c7516847c494e2d7e0d88f83c71
Chansong <quickfailer@gmail.com>
2025-01-10 15:16:49 +0900

    refactored extrac_batch_dag.py

--- dags/extract_batch_dag.py
+++ dags/extract_batch_dag.py
@@ -1,4 +1,3 @@
-import time
 import datetime
 from airflow import DAG
 from airflow.decorators import task_group
@@ -17,7 +16,7 @@ from upload_data.upload_to_s3 import upload_to_s3
 ################################################
 
 # 야후 파이낸스 API 추출용 심볼 및 S3 저장 경로 정의
-yf_symbols_and_keys = [
+YAHOO_FINANCE_SYMBOLS_N_KEYS = [
     ('^GSPC', 'raw/s&p500.csv'),
     ('^IXIC', 'raw/nasdaq.csv'),
     ('GC=F', 'raw/gold.csv'),
@@ -25,22 +24,15 @@ yf_symbols_and_keys = [
     ('^VIX', 'raw/vix.csv'),
 ]
 
-# DAG 설정
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime.datetime(2024, 12, 23),
-    'retries': 1,
-}
-
 # 바이낸스 데이터 추출 후 업로드 시키는 함수
-def upload_binance_task(ti):
+def upload_binance_batch_data(ti):
     upload_to_s3(
         data=ti.xcom_pull(task_ids='binance.fetch_binance_batch', key='binance_batch'),
         s3_key="raw/btc_usdt_data.csv"
     )
 
-# 야후 파이낸스스 데이터 추출 후 업로드 시키는 함수
-def upload_yf_task(ti, yf_symbol, s3_key):
+# 야후 파이낸스 데이터 추출 후 업로드 시키는 함수
+def upload_yahoo_batch_data(ti, yf_symbol, s3_key):
     upload_to_s3(
         data=ti.xcom_pull(
             task_ids=f"yf.fetch_yf_batch_{yf_symbol.replace('^', '').replace('=', '')}",
@@ -52,20 +44,24 @@ def upload_yf_task(ti, yf_symbol, s3_key):
 # DAG 정의
 with DAG(
     dag_id = 'extract_batch_dag',
-    default_args=default_args,
+    default_args={
+    'owner': 'airflow',
+    'start_date': datetime.datetime(2024, 12, 23),
+    'retries': 1,
+    },
     description='A DAG to fetch batch data and upload to S3',
     schedule_interval='0 10 * * *',
     catchup=False,
     tags=['batch', 'S3'],
 ) as dag:
 
-    # DummyOperator로 start와 end 정의
+    # DummyOperator로 DAG의 start와 end 정의
     start = DummyOperator(task_id='start')
     end = DummyOperator(task_id='end')
 
     # 바이낸스 데이터 추출 -> 업로드 Task Group
     @task_group
-    def binance():
+    def binance_batch_data_task_group():
         fetch_binance_batch_task = PythonOperator(
             task_id='fetch_binance_batch',
             python_callable=fetch_binance_batch,
@@ -73,38 +69,38 @@ with DAG(
 
         upload_binance_batch_task = PythonOperator(
             task_id='upload_binance_batch',
-            python_callable=upload_binance_task,
+            python_callable=upload_binance_batch_data,
         )
 
         fetch_binance_batch_task >> upload_binance_batch_task
 
     # 야후 파이낸스 데이터 추출 -> 업로드 Task Group
     @task_group
-    def yf():
-        for yf_symbol, yf_s3_key in yf_symbols_and_keys:
-            sanitized_symbol = yf_symbol.replace("^", "").replace("=", "")
-            fetch_yf_task_id = f'fetch_yf_batch_{sanitized_symbol}'
-            upload_yf_task_id = f'upload_yf_batch_{sanitized_symbol}'
+    def yahoo_batch_data_task_group():
+        for YAHOO_SYMBOL, YAHOO_KEY in YAHOO_FINANCE_SYMBOLS_N_KEYS:
+            sanitized_symbol = YAHOO_SYMBOL.replace("^", "").replace("=", "")
+            fetch_yahoo_task_id = f'fetch_yf_batch_{sanitized_symbol}'
+            upload_yahoo_task_id = f'upload_yf_batch_{sanitized_symbol}'
 
-            fetch_yf_batch_task = PythonOperator(
-                task_id=fetch_yf_task_id,
+            fetch_yahoo_batch_task = PythonOperator(
+                task_id=fetch_yahoo_task_id,
                 python_callable=fetch_yf_batch,
-                op_kwargs={'symbol': yf_symbol},
+                op_kwargs={'symbol': YAHOO_SYMBOL},
             )
 
-            upload_yf_batch_task = PythonOperator(
-                task_id=upload_yf_task_id,
-                python_callable=upload_yf_task,
-                op_kwargs={'yf_symbol': yf_symbol, 's3_key': yf_s3_key},
+            upload_yahoo_batch_task = PythonOperator(
+                task_id=upload_yahoo_task_id,
+                python_callable=upload_yahoo_batch_data,
+                op_kwargs={'yf_symbol': YAHOO_SYMBOL, 's3_key': YAHOO_KEY},
             )
 
-            fetch_yf_batch_task >> upload_yf_batch_task
+            fetch_yahoo_batch_task >> upload_yahoo_batch_task
 
-    # daily 배치(훈련) 데이터 로드 작업 완료시 training DAG를 Trigger
+    # 배치 데이터 로드 작업 완료시 training DAG를 Trigger
     trigger_training_task = TriggerDagRunOperator(
         task_id='trigger_training',
-        trigger_dag_id='training_dag',  # 트리거할 DAG ID
+        trigger_dag_id='training_dag',
     )
 
-    # 전체 Task 종속성
-    start >> [binance(), yf()] >> trigger_training_task >> end
+    # 전체 Task Flow
+    start >> [binance_batch_data_task_group(), yahoo_batch_data_task_group()] >> trigger_training_task >> end
